5b6e6231344d93f226ab25fd2093780afea2c475
gustavoalencar1 <100171910+gustavoalencar1@users.noreply.github.com>
2023-10-16 19:44:37 -0300

    Comit

--- Busca binaria/BuscaBinariaRecursiva.java
+++ Busca binaria/BuscaBinariaRecursiva.java
@@ -1,12 +1,35 @@
 import javax.swing.JOptionPane;
 
-public class BuscaBinariaRecusriva {
-    public static void main (String args[])
-    {
-        int iVetor[]´= new int[] {10, 20, 30, 40, 50, 60};
-        int iIndice,iK;
+public class BuscaBinariaRecursiva {
+    // Função de pesquisa binária recursiva
+    public static boolean binarySearch(int[] arr, int key, int left, int right) {
+        if (left > right) {
+            return false;
+        }
         
-        Ik = integer.parseInt(JOptionPane.showInputDialog ("Digite a chave de busca:"))
-        
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == key) {
+            return true; // Key found
+        } else if (arr[mid] < key) {
+            return binarySearch(arr, key, mid + 1, right); // Pesquise na metade direita
+        } else {
+            return binarySearch(arr, key, left, mid - 1); // Pesquise na metade esquerda
+        }
+    }
+
+    public static void main(String args[]) {
+        int iVetor[] = new int[] {10, 20, 30, 40, 50, 60};
+        int iK;
+
+        iK = Integer.parseInt(JOptionPane.showInputDialog("Digite a chave de busca:"));
+
+        // Chama toda a função de busca binária e exibe o resultado
+        boolean found = binarySearch(iVetor, iK, 0, iVetor.length - 1);
+        if (found) {
+            JOptionPane.showMessageDialog(null, "Chave encontrada.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Chave não encontrada.");
+        }
     }
 }
